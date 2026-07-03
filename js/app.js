73340db75b3d85846aee858a2503da73f029abce
cre8vX <yasithanjana2002@gmail.com>
2026-07-04 04:49:20 +0530

    Comment out initial JavaScript code and add logical operator example

--- js/app.js
+++ js/app.js
@@ -1,11 +1,67 @@
-let name = "Saman";
+// let name = "Saman";
+// let age = 18;
+// name = "Ranil";
 
-let age = 18;
+// console.log(name);
+// console.log(age);
+// console.log(typeof name);
+// alert("Hello iCET");
 
-name = "Ranil";
+// ----------------------------------------------------------------------------------------------------------
 
-console.log(name);
-console.log(age);
+// let name = "Saman";
+// let age = 18;
+// let isAfterAL = false;
+// let customers = []; // array
+// let customer = {}; // object
+// let salary = 7500.50;
 
-alert("Hello iCET");
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isAfterAL);
+// console.log(typeof customers);
+// console.log(typeof customer);
+// console.log(typeof salary);
+
+// ----------------------------------------------------------------------------------------------------------
+
+// Arithmetic Operators (+ - * / %)
+
+// let x = 10;
+// let y = 20;
+
+// let z = x + y;
+
+// console.log(z);
+
+// ----------------------------------------------------------------------------------------------------------
+
+// Comparison Operators (== != === !== > < >= <=)
+
+// let x = 10;
+// let y = 10; 
+// console.log(x == y); // true
+
+// let x = 10;
+// let y = "10"; 
+// console.log(x == y); // true
+
+// let x = 10;
+// let y = "11"; 
+// console.log(x == y); // false
+
+// let x = 10;
+// let y = "10"; 
+// console.log(x === y); // false
+
+//----------------------------------------------------------------------------------------------------------
+
+// Logical Operators (&& || !)
+
+// if (true && true) {
+//     console.log("Both are true");
+// } 
+
+//----------------------------------------------------------------------------------------------------------
+
 
